13903b0b9d7b2b23fd19126c70b3a6ccdf40bf26
Ajish <ajish.r@infospica.com>
2023-10-17 18:04:17 +0530

    ss

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,14 +4,14 @@ export default function Filters() {
   const [selectedItem, setSelectedItem] = useState(1);
   return (
     <section className="my-10 py-3 md:w-3/6 w-2/5 mx-auto flex justify-around flex-col md:flex-row sticky">
-      <a href="#latest"><button
+      <button
         className={`buttonStyle ${
           selectedItem === 1 ? "bg-green-400 text-white" : "text-slate-500"
         }`}
         onClick={() => setSelectedItem(1)}
       >
       Latest
-      </button></a>
+      </button>
       <button className={`buttonStyle ${
           selectedItem === 2 ? "bg-green-400 text-white" : "text-slate-500"
         }`} onClick={() => setSelectedItem(2)}>
